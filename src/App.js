eb36a2225de20154ef2637981f0d7ee8ff7146da
Kelly Innes <kinnes@gmail.com>
2020-11-27 18:28:49 -0500

    Create initial version

--- src/App.js
+++ src/App.js
@@ -1,24 +1,83 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import _ from 'lodash';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+
+import translate from './translate';
+
 import './App.css';
 
+const DEFAULT_INPUT_STRING = '';
+const TEXT_AREA_ROWS = 12;
+
 function App() {
+  const [stringToTranslate, setStringToTranslate] = useState(DEFAULT_INPUT_STRING);
+
+  const handleInputChange = (e) => {
+    const updatedInput = _.get(e, 'target.value');
+
+    setStringToTranslate(updatedInput);
+  };
+
+  const resetInput = () => setStringToTranslate(DEFAULT_INPUT_STRING);
+
+  const translation = translate(stringToTranslate);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <main className="App">
+      <div className="header">
+        <Typography variant="h5" align="center">
+          toki-pona-translator
+        </Typography>
+      </div>
+      <div className="textFieldContainer">
+        <TextField
+          autoFocus
+          label="English"
+          id="english-input"
+          value={stringToTranslate}
+          onChange={handleInputChange}
+          variant="outlined"
+          fullWidth
+          multiline
+          rows={TEXT_AREA_ROWS}
+          rowsMax={TEXT_AREA_ROWS}
+        />
+        <div className="spacer"></div>
+        <TextField
+          label="Toki Pona"
+          id="toki-pona-output"
+          value={translation}
+          disabled
+          variant="outlined"
+          fullWidth
+          multiline
+          rows={TEXT_AREA_ROWS}
+          rowsMax={TEXT_AREA_ROWS}
+        />
+      </div>
+      <div className="buttonContainer">
+        <CopyToClipboard text={translation}>
+          <Button
+            color="primary"
+            variant="contained"
+            disableElevation
+          >
+            Copy translation
+          </Button>
+        </CopyToClipboard>
+        <Button
+          onClick={resetInput}
+          color="secondary"
+          variant="contained"
+          disableElevation
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Reset input
+        </Button>
+      </div>
+    </main>
   );
 }
 
